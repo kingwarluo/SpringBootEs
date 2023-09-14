d897e1a835c5ac1042c57a765f386acc08278001
luojianhua <kingwarluo@gmail.com>
2023-09-14 09:40:38 +0800

    controller

--- src/main/java/com/kingwarluo/match/controller/MatchController.java
+++ src/main/java/com/kingwarluo/match/controller/MatchController.java
@@ -12,11 +12,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.AsyncContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
@@ -50,8 +58,43 @@ public class MatchController {
         }
     }
 
-    @RequestMapping("/cancel/{id}")
+    @GetMapping("/cancel/{id}")
     public boolean cancel(@PathVariable("id") Long id) {
         return matchService.cancel(id);
     }
+
+
+    @GetMapping("/async")
+    public void async(HttpServletRequest req) {
+        System.out.println(new Date());
+        AsyncContext asyncContext = req.startAsync();
+        asyncContext.setTimeout(0L);
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1, new ThreadFactory() {
+            @Override
+            public Thread newThread(Runnable r) {
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.setName("com.alibaba.nacos.LongPolling");
+                return t;
+            }
+        });
+        ScheduledFuture<?> future = scheduler.schedule(() -> {
+            Date now = new Date();
+            System.out.println(now);
+            HttpServletResponse response = (HttpServletResponse)asyncContext.getResponse();
+            // 禁用缓存
+            response.setHeader("Pragma", "no-cache");
+            response.setDateHeader("Expires", 0);
+            response.setHeader("Cache-Control", "no-cache,no-store");
+            response.setStatus(HttpServletResponse.SC_OK);
+            try {
+                response.getWriter().println(now);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            asyncContext.complete();
+        }, 20, TimeUnit.SECONDS);
+    }
+
 }
